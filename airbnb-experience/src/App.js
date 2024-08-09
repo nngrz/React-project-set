d939efc6457c9ebfdc9d977d2d20640a76d8c2fe
Ningrui Zhang <ningrui1227@gmail.com>
2024-08-09 23:35:12 +0100

    update airbnb-experience

---
+++ airbnb-experience/src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+/*
+Challenge: Build the Navbar component.
+Check the Figma file for the design specifics.
+*/
+
+export default function App() {
+        // <Hero />
+    return (
+        <div>
+            <Navbar />
+            <Card />
+        </div>
+    )
+}
