e32ad33ebbb2452985dee3f35e58b1ef678ffe71
Ningrui Zhang <ningrui1227@gmail.com>
2024-08-11 12:37:27 +0100

    update digital_business_card

--- digital_business_card/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import DigitalCard from "./DigitalCard"
-
-ReactDOM.createRoot(document.getElementById("root")).render(<DigitalCard />)
