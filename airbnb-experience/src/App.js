c1a0cafa3cb4f6287d33b23007e764aa81f0a55d
Ningrui Zhang <ningrui1227@gmail.com>
2024-08-17 19:57:56 +0100

    update airbnb-experience

--- airbnb-experience/src/App.js
+++ airbnb-experience/src/App.js
@@ -2,17 +2,26 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
-/*
-Challenge: Build the Navbar component.
-Check the Figma file for the design specifics.
-*/
+import data from "./data"
+
 
 export default function App() {
         // <Hero />
+    const cardElements = data.map(info => {
+        return <Card coverImg={info.coverImg} rating={info.statsrating} reviewCount={info.reviewCount} location={info.location} title={info.title} price={info.price}/>
+    })
     return (
         <div>
             <Navbar />
-            <Card />
+            {cardElements}
+            {/* <Card 
+                img="katie-zaferes.png"
+                rating="5.0"
+                reviewCount={6}
+                location="USA"
+                title="Life Lessons with Katie Zaferes"
+                price={136}
+            /> */}
         </div>
     )
 }
--- airbnb-experience/src/components/Card.js
+++ airbnb-experience/src/components/Card.js
@@ -1,38 +1,17 @@
 import React from "react"
 
-/*
-Challenge: Build the Card component
-For now, hard-code in the data (like 
-the rating, title, price, etc.)
-
-Notes:
-- Only render 1 instance (I already did this for you)
-- The star icon and photo (katie-zaferes.png) are in the images 
-  folder for your use
-- Make sure to include:
-    - image
-    - star icon (star.png), rating, and review count
-    - title
-    - cost/person
-- The main purpose of this challenge is to show you where our limitations
-  currently are, so don't worry about the fact that you're hard-coding all
-  this data into the component.
-*/
-
-export default function Card() {
+export default function Card(props) {
     return (
-        <section>
-            <img src="../images/katie-zaferes.png"></img>
-            <div>
-                <img src="../images/star.png"></img>
-                <p>rating</p>
-                <p>review count</p>
+        <div className="card">
+            <img src={`../images/${props.coverImg}`} className="card--image" />
+            <div className="card--stats">
+                <img src="../images/star.png" className="card--star" />
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.location}</span>
             </div>
-            <p>title</p>
-            <div>
-                <p>cost</p>
-                <p>/ person</p>
-            </div>
-        </section>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
+        </div>
     )
 }
