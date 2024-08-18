c0023067d3c5f9a2c7b1ed085715cc0428faa6ba
Ningrui Zhang <ningrui1227@gmail.com>
2024-08-18 10:17:33 +0100

    update airbnb-experience

--- airbnb-experience/src/App.js
+++ airbnb-experience/src/App.js
@@ -4,24 +4,23 @@ import Hero from "./components/Hero"
 import Card from "./components/Card"
 import data from "./data"
 
-
 export default function App() {
-        // <Hero />
-    const cardElements = data.map(info => {
-        return <Card coverImg={info.coverImg} rating={info.statsrating} reviewCount={info.reviewCount} location={info.location} title={info.title} price={info.price}/>
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                item={item}
+            />
+        )
     })
+
     return (
         <div>
             <Navbar />
-            {cardElements}
-            {/* <Card 
-                img="katie-zaferes.png"
-                rating="5.0"
-                reviewCount={6}
-                location="USA"
-                title="Life Lessons with Katie Zaferes"
-                price={136}
-            /> */}
+            <Hero />
+            <section className="cards-list">
+                {cards}
+            </section>
         </div>
-    )
+    )   
 }
