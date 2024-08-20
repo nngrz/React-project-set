798cdca0f021ab68376ae8d319727f7e60c817d1
Ningrui Zhang <ningrui1227@gmail.com>
2024-08-20 13:08:09 +0100

    destruct props

--- travel_journal/src/components/About.js
+++ travel_journal/src/components/About.js
@@ -2,21 +2,21 @@ import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons"
 
-export default function About(props) {
+export default function About({item}) {
     return ( 
         <div className="about--container">
-            <img src={`../images/${props.item.image}`} className="about--img" />
+            <img src={`../images/${item.image}`} className="about--img" />
             <div className="info--container">
                 <div className="location--container">
                     <div className="location--container--small">
                         <FontAwesomeIcon icon={faLocationDot} className="location--icon" />
-                        <h4 className="location--country">{props.item.country}</h4>
+                        <h4 className="location--country">{item.country}</h4>
                     </div>
-                    <a href={props.item.googleMapsUrl} className="location--map">View on Google Maps</a>
+                    <a href={item.googleMapsUrl} className="location--map">View on Google Maps</a>
                 </div>
-                <h2 className="info--name">{props.item.name}</h2>
-                <h3 className="info--date">{props.item.startDate} - {props.item.endDate}</h3>
-                <p className="info--description">{props.item.description}</p>
+                <h2 className="info--name">{item.name}</h2>
+                <h3 className="info--date">{item.startDate} - {item.endDate}</h3>
+                <p className="info--description">{item.description}</p>
             </div>
         </div>
     )
