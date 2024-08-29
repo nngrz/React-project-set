69daee529c310306f63143acaa3f5ea3f1866d6a
Ningrui Zhang <ningrui1227@gmail.com>
2024-08-29 19:19:56 +0100

    add light_dark_mode

---
+++ light_dark_mode/src/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
+
+export default function App() {
+    const [darkMode, setDarkMode] = React.useState(false)
+
+    function toggleMode() {
+        console.log("clicked")
+        setDarkMode (prevState => !prevState)
+    }
+
+    return (
+        <div className="container">
+            <Navbar darkMode={darkMode} toggleDarkMode={toggleMode}/>
+            <Main darkMode={darkMode}/>
+        </div>
+    )
+}
