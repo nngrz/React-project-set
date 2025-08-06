432e4570cc15d50bd597f62198a117500b51b6f5
Ningrui Zhang <ningrui1227@gmail.com>
2025-08-06 16:47:40 +0100

    update code to use the dynamic port provided by Railway

--- notes_app/backend/index.js
+++ notes_app/backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT ||3001;
 
 app.use(cors());
 app.use(express.json());
